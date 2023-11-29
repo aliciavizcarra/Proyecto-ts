955de1540aad3dafc38f20f6a9d1c318479f5c3c
Alicia Vizcarra <220036@fppiramide.com>
2023-11-29 10:19:29 +0100

    ya

--- proyectos/domain/Proyecto.ts
+++ proyectos/domain/Proyecto.ts
@@ -1,4 +1,5 @@
 export default interface Proyecto {
     id?:string;
     name: string;
+    
 }
---
+++ proyectos/domain/Tarea.ts
@@ -0,0 +1,14 @@
+class Tarea{
+
+    codigo: String;
+    nombre: String;
+    proyecto: number;
+    especialidad: String;
+    especialista: number;
+
+    constructor(codigo: String, nombre: String, proyecto: number)
+
+
+
+
+}
